209e31cad70921acbf3e95c35ad13eee27937963
Lucas Moreno Silva <lucasmorenosilva@gmail.com>
2018-09-25 03:46:05 -0300

    Add seeds

---
+++ priv/repo/seeds/project_seeds.exs
@@ -0,0 +1,17 @@
+alias ProjectManagementExample.Repo
+alias ProjectManagementExample.Management.Project
+
+Repo.insert! %Project{
+  title: "Super Cool Project",
+  description: "This project is the best project ever"
+}
+
+Repo.insert! %Project{
+  title: "Super Cool Project",
+  description: "This project is the best project ever"
+}
+
+Repo.insert! %Project{
+  title: "Super Cool Project",
+  description: "This project is the best project ever"
+}
